0e965c29777c095eada74c9667aaec4154cb43ea
LoucaColes <coleslouca@gmail.com>
2019-08-19 19:33:49 +0100

    Main functionality implemented and added a custom inspector for the class.

---
+++ Screenshot Tool/Assets/Classes/ScreenCaptureTool.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LC.Tools.ScreenCapture
+{
+    public class ScreenCaptureTool : MonoBehaviour
+    {
+        public static ScreenCaptureTool instance; // Singleton
+
+        // Normal Settings
+        [SerializeField, Tooltip("The file name of the screenshot")] private string fileName = "Screenshot";
+        [SerializeField, Range(1, 5),
+            Tooltip("Upscale the initial image by a factor of x")] private int upScale = 1;
+
+        // Advanced Settings
+        [SerializeField, 
+            Tooltip("Recommended for sequencial screenshots")] private bool useTime = false;
+        [SerializeField, Tooltip("The file type of the screenshot")] private FileType fileType = FileType.PNG;
+
+
+        /// <summary>
+        /// Take screenshot without passing in any settings
+        /// </summary>
+        [ContextMenu("Take Screenshot")]
+        public void TakeScreenshot()
+        {
+            // Set the file name
+            string file = fileName;
+
+            // If we are using time to modify the file name
+            if (useTime)
+            {
+                // Get the current time in string form
+                string time = System.DateTime.Now.ToLongTimeString();
+                
+                // Replace all : with _
+                time = time.Replace(':', '_');
+
+                // Add the current time to the file name
+                file += "_" + time;
+            }
+            
+            // Add the correct file extension to the file name
+            file += DetermineFileType(fileType);
+
+            // Take the screenshot
+            UnityEngine.ScreenCapture.CaptureScreenshot(file, upScale);
+        }
+
+        /// <summary>
+        /// Take screenshot with some settings
+        /// </summary>
+        /// <param name="_fileName">Name of the file</param>
+        public void TakeScreenshot(string _fileName)
+        {
+            // Set the file name
+            string file = _fileName;
+
+            // If we are using time to modify the file name
+            if (useTime)
+            {
+                // Get the current time in string form
+                string time = System.DateTime.Now.ToLongTimeString();
+
+                // Replace all : with _
+                time = time.Replace(':', '_');
+
+                // Add the current time to the file name
+                file += "_" + time;
+            }
+
+            // Add the correct file extension to the file name
+            file += DetermineFileType(fileType);
+
+            // Take the screenshot
+            UnityEngine.ScreenCapture.CaptureScreenshot(file, upScale);
+        }
+
+        /// <summary>
+        /// Take screenshot with some settings
+        /// </summary>
+        /// <param name="_fileName">Name of the file</param>
+        /// <param name="_useTime">Is the current time addd to the file name</param>
+        public void TakeScreenshot(string _fileName, bool _useTime)
+        {
+            // Set the file name
+            string file = _fileName;
+
+            // If we are using time to modify the file name
+            if (_useTime)
+            {
+                // Get the current time in string form
+                string time = System.DateTime.Now.ToLongTimeString();
+
+                // Replace all : with _
+                time = time.Replace(':', '_');
+
+                // Add the current time to the file name
+                file += "_" + time;
+            }
+
+            // Add the correct file extension to the file name
+            file += DetermineFileType(fileType);
+
+            // Take the screenshot
+            UnityEngine.ScreenCapture.CaptureScreenshot(file, upScale);
+        }
+
+        /// <summary>
+        /// Take screenshot with some settings
+        /// </summary>
+        /// <param name="_fileName">Name of the file</param>
+        /// <param name="_upscale">The value the image is upscaled by</param>
+        public void TakeScreenshot(string _fileName, int _upscale)
+        {
+            // Set the file name
+            string file = _fileName;
+
+            // If we are using time to modify the file name
+            if (useTime)
+            {
+                // Get the current time in string form
+                string time = System.DateTime.Now.ToLongTimeString();
+
+                // Replace all : with _
+                time = time.Replace(':', '_');
+
+                // Add the current time to the file name
+                file += "_" + time;
+            }
+
+            // Add the correct file extension to the file name
+            file += DetermineFileType(fileType);
+
+            // Take the screenshot
+            UnityEngine.ScreenCapture.CaptureScreenshot(file, _upscale);
+        }
+
+        /// <summary>
+        /// Take screenshot with some settings
+        /// </summary>
+        /// <param name="_fileName">Name of the file</param>
+        /// <param name="_upscale">The value the image is upscaled by</param>
+        /// <param name="_useTime">Is the current time addd to the file name</param>
+        public void TakeScreenshot(string _fileName, int _upscale, bool _useTime)
+        {
+            // Set the file name
+            string file = _fileName;
+
+            // If we are using time to modify the file name
+            if (_useTime)
+            {
+                // Get the current time in string form
+                string time = System.DateTime.Now.ToLongTimeString();
+
+                // Replace all : with _
+                time = time.Replace(':', '_');
+
+                // Add the current time to the file name
+                file += "_" + time;
+            }
+
+            // Add the correct file extension to the file name
+            file += DetermineFileType(fileType);
+
+            // Take the screenshot
+            UnityEngine.ScreenCapture.CaptureScreenshot(file, _upscale);
+        }
+
+        /// <summary>
+        /// Reset the current settings to the defaults
+        /// </summary>
+        public void ResetSettings()
+        {
+            // TODO: Make sure that all settings are reset
+            fileName = "Screenshot";
+            upScale = 1;
+            useTime = false;
+            fileType = FileType.PNG;
+        }
+
+        /// <summary>
+        /// Determines what the file type is and returns it's respective file extension in string form
+        /// </summary>
+        /// <param name="_type">File type</param>
+        /// <returns>File extension in string form</returns>
+        private string DetermineFileType(FileType _type)
+        {
+            switch(_type)
+            {
+                case FileType.PNG:
+                    return ".png";
+                case FileType.JPEG:
+                    return ".jpeg";
+                case FileType.BMP:
+                    return ".bmp";
+                case FileType.GIF:
+                    return ".gif";
+                default: // If not a standard image file format return empty string and log error
+                    Debug.LogError("Screenshot - Invalid File Type");
+                    return "";
+            }
+        }
+    }
+
+    public enum FileType
+    {
+        PNG,
+        JPEG,
+        BMP,
+        GIF,
+    }
+}
---
+++ Screenshot Tool/Assets/Editor/ScreenCaptureToolEditor.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace LC.Tools.ScreenCapture
+{
+    [CustomEditor(typeof(ScreenCaptureTool))]
+    public class ScreenCaptureToolEditor : Editor
+    {
+        ScreenCaptureTool captureTool;
+
+        // Properties
+        // Normal Settings
+        private SerializedProperty fileName;
+        private SerializedProperty upscale;
+
+        // Advanced Settings
+        private SerializedProperty useTime;
+        private SerializedProperty fileType;
+
+        // Foldouts
+        private bool settingsFoldout = true;
+        private bool advancedSettingsFoldout = true;
+
+        // Styles
+        private Color originalBackgroundColour;
+        private GUIStyle screenshotButtonStyle;
+
+        /// <summary>
+        /// Set up references and variables
+        /// </summary>
+        void OnEnable()
+        {
+            // Get a reference to the target
+            captureTool = (ScreenCaptureTool)target;
+
+            // Get all of the properties from the target class
+            // Settings
+            fileName = serializedObject.FindProperty("fileName");
+            upscale = serializedObject.FindProperty("upScale");
+
+            // Advanced Settings
+            useTime = serializedObject.FindProperty("useTime");
+            fileType = serializedObject.FindProperty("fileType");
+
+            
+        }
+
+        /// <summary>
+        /// Update the inspector GUI
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            // Update the serialised object
+            serializedObject.Update();
+
+            // Get a copy of the original GUI background colour and set the GUI background colour to it
+            originalBackgroundColour = GUI.backgroundColor;
+            GUI.backgroundColor = originalBackgroundColour;
+
+            // Display the settings foldout
+            settingsFoldout = EditorGUILayout.Foldout(settingsFoldout, "Settings");
+            if (settingsFoldout) // If foldout is true
+            {
+                // Display the file name property field
+                EditorGUILayout.PropertyField(fileName);
+                if (fileName.stringValue.Length >= 10) // If the file name is >= 10
+                {
+                    // Display warning message about being too long
+                    EditorGUILayout.HelpBox("File name is getting long!", MessageType.Warning);
+                }
+
+                // Display the upscale property
+                EditorGUILayout.PropertyField(upscale);
+                if (upscale.intValue > 1) // If the upscale value is > 1
+                {
+                    // Display a warning message about framerate issues
+                    EditorGUILayout.HelpBox("Setting the upscale value to be greater than 1 can cause framerate issues", MessageType.Warning);
+                }
+            }
+
+            // Display the advanced settings  foldout
+            advancedSettingsFoldout = EditorGUILayout.Foldout(advancedSettingsFoldout, "Advanced Settings");
+            if (advancedSettingsFoldout) // If foldout is true
+            {
+                // Display the use time property
+                EditorGUILayout.PropertyField(useTime);
+                if (!useTime.boolValue) // If use time is false
+                {
+                    // Display warning message
+                    EditorGUILayout.HelpBox("If taking sequential or a group of screenshots, this will be useful as otherwise screenshots " +
+                        "could be overwritten",
+                        MessageType.Warning);
+                }
+
+                // Display the file type property field
+                EditorGUILayout.PropertyField(fileType);
+                if (fileType.enumValueIndex == 3) // If gif file type
+                {
+                    // Display warning message
+                    EditorGUILayout.HelpBox("Warning: The gif won't be animated!", MessageType.Warning);
+                }
+            }
+
+            // Apply the changes to the serialised object
+            serializedObject.ApplyModifiedProperties();
+
+            // Set the background colour to red
+            GUI.backgroundColor = Color.red;
+
+            // Set up screenshot button style
+            screenshotButtonStyle = new GUIStyle(GUI.skin.button);
+            screenshotButtonStyle.normal.textColor = Color.white;
+
+            // Display take screenshot button
+            if (GUILayout.Button("Take Screenshot", screenshotButtonStyle))
+            {
+                // If button pressed take screenshot
+                captureTool.TakeScreenshot();
+            }
+
+            // Reset the GUI background colour
+            GUI.backgroundColor = originalBackgroundColour;
+
+            // Display the reset settings button
+            if (GUILayout.Button("Reset Settings"))
+            {
+                // If button pressed reset the settings
+                captureTool.ResetSettings();
+            }
+
+            // Display the help button
+            if (GUILayout.Button("Help!"))
+            {
+                // If button pressed create a new help window
+                // TODO: Create help window class
+                // TODO: Check if there is already a window and if so don't create new window
+                Debug.LogError("Louca has forgotten to implement/link this functionality - Help Button");
+            }
+        }
+    }
+}
